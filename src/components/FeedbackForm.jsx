684233186857f707abd33e32d448c2302591d309
Kevin Tan <kevintan@Kevins-MacBook-Pro-2.local>
2022-05-02 23:14:06 -0700

    added Card shared style component and also feedback form input

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Card from './shared/Card';
+import { useState } from 'react';
+import Button from './shared/Button';
+
+function FeedbackForm() {
+  const [text, setText] = useState('');
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        <div className='input-group'>
+          <input
+            type='text'
+            onChange={handleTextChange}
+            placeholder='Write a review'
+            value={text}
+          />
+          <Button type='submit'>Send</Button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,24 @@
+import { isDisabled } from '@testing-library/user-event/dist/utils';
+import React from 'react';
+
+function Button({ children, version, type, isDisabled }) {
+  return (
+    <div>
+      <button
+        type={type}
+        disabled={isDisabled}
+        className={`btn btn-${version}`}
+      >
+        {children}
+      </button>
+    </div>
+  );
+}
+
+Button.defaultProps = {
+  version: 'primary',
+  type: 'button',
+  isDisabled: false,
+};
+
+export default Button;
